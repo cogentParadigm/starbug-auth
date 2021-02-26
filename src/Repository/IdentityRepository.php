eae0d9289ff68bcdfd4dffe7ce15e022499ec58f
Ali Gangji <ali@neonrain.com>
2021-02-26 15:01:01 -0700

    Fix null returns from IdentityRepository

--- src/Repository/IdentityRepository.php
+++ src/Repository/IdentityRepository.php
@@ -14,12 +14,13 @@ class IdentityRepository implements IdentityRepositoryInterface {
     $query = $this->db->query("users")
       ->condition("users.deleted", "0")
       ->select("users.*")
-      ->select("GROUP_CONCAT(users.groups.slug) as groups");
+      ->select("GROUP_CONCAT(users.groups.slug) as groups")
+      ->group("users.id");
     if (is_array($criteria)) $query->conditions($criteria);
     else $query->condition("users.id", $criteria);
     $user = $query->one();
-    if (!is_array($user['groups'])) $user['groups'] = is_null($user['groups']) ? [] : explode(",", $user['groups']);
     if ($user) {
+      if (!is_array($user['groups'])) $user['groups'] = is_null($user['groups']) ? [] : explode(",", $user['groups']);
       $identity = new Identity($user["id"], $user["password"], $user["groups"]);
       unset($user["password"]);
       $identity->setData($user);
